1741ff828edb2aa8c18058fcf8eaa3417a374bb3
Charlie_H <c.harpur@hotmail.com>
2023-01-04 12:29:33 -0500

    Configured Discord Rich Presence

--- after/plugin/presence.lua
+++ after/plugin/presence.lua
@@ -1,5 +1,11 @@
 require('presence'):setup({
     -- General Options
+    main_image = 'file',
     neovim_image_text = "Hi :)",
     show_time = false,
+
+    -- Rich Presence Text
+    editing_text = 'Editing "%s"',
+    reading_text = 'Reading "%s"',
+    workspace_text = 'Working on "%s"',
 })
--- plugin/packer_compiled.lua
+++ plugin/packer_compiled.lua
@@ -196,6 +196,10 @@ _G.packer_plugins = {
 }
 
 time([[Defining packer_plugins]], false)
+-- Config for: rose-pine
+time([[Config for rose-pine]], true)
+try_loadstring("\27LJ\2\n9\0\0\3\0\3\0\0056\0\0\0009\0\1\0'\2\2\0B\0\2\1K\0\1\0\26colorscheme rose-pine\bcmd\bvim\0", "config", "rose-pine")
+time([[Config for rose-pine]], false)
 -- Config for: lualine.nvim
 time([[Config for lualine.nvim]], true)
 try_loadstring("\27LJ\2\n5\0\0\3\0\3\0\0066\0\0\0'\2\1\0B\0\2\0029\0\2\0B\0\1\1K\0\1\0\nsetup\flualine\frequire\0", "config", "lualine.nvim")
@@ -204,10 +208,6 @@ time([[Config for lualine.nvim]], false)
 time([[Config for gitsigns.nvim]], true)
 try_loadstring("\27LJ\2\n6\0\0\3\0\3\0\0066\0\0\0'\2\1\0B\0\2\0029\0\2\0B\0\1\1K\0\1\0\nsetup\rgitsigns\frequire\0", "config", "gitsigns.nvim")
 time([[Config for gitsigns.nvim]], false)
--- Config for: rose-pine
-time([[Config for rose-pine]], true)
-try_loadstring("\27LJ\2\n9\0\0\3\0\3\0\0056\0\0\0009\0\1\0'\2\2\0B\0\2\1K\0\1\0\26colorscheme rose-pine\bcmd\bvim\0", "config", "rose-pine")
-time([[Config for rose-pine]], false)
 
 _G._packer.inside_compile = false
 if _G._packer.needs_bufread == true then
